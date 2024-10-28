eb5ae34e9f7ebbb778e70d300791b138449ca480
yangyunpeng <331047418@qq.com>
2024-10-28 18:53:52 +0800

    feat: gis接口

--- src/api/modules/gis.ts
+++ src/api/modules/gis.ts
@@ -1,30 +1,139 @@
 import api from '../index'
 import type { GeneralQuery, RegionQuery } from './queryTypes'
-import type { AirStation, Meteorology, OverallWaterQualityStats, ProvinceCityTree, SeaWaterOverallStats, WaterQualityComplianceResult, WaterYoyProportion } from './resultTypes'
+import type {
+  AirStation,
+  Meteorology,
+  OutfallGoalStats,
+  OutfallOverallStats,
+  OutfallTypeStats,
+  OverallWaterQualityStats,
+  ProvinceCityTree,
+  SeaWaterEutrophication,
+  SeaWaterOverallStats,
+  SeaWaterQualityCompliance,
+  SeaWaterQualityTrend,
+  SeaWaterStats,
+  WaterQualityComplianceResult,
+  WaterQualityDetail,
+  WaterQualityTrend,
+  WaterYoyProportion,
+} from './resultTypes'
 
 export default {
-  // 获取并计算生成海水水质等级面积数据
+  /**
+   * 获取并计算生成海水水质等级面积数据
+   * @param query
+   * @returns
+   */
   getRegion: (query: RegionQuery): Promise<ProvinceCityTree[]> => api.post('/app/data/gis-data/region', query),
 
-  // 驾驶舱-入海河流(左上)
-  getRiverOverall: (query: GeneralQuery): Promise<{
-    sea: OverallWaterQualityStats// 东海区
-    battle: OverallWaterQualityStats// 攻坚战
+  /**
+   * 驾驶舱-入海河流(左上)
+   * @param query
+   * @returns
+   */
+  getRiverOverall: (
+    query: GeneralQuery,
+  ): Promise<{
+    sea: OverallWaterQualityStats // 东海区
+    battle: OverallWaterQualityStats // 攻坚战
   }> => api.post('/app/data/gis-data/river-overall', query),
 
-  // 驾驶舱-大气实况(左下)
+  /**
+   * 驾驶舱-大气实况(左下)
+   * @param query
+   * @returns
+   */
   getAirStation: (query: GeneralQuery): Promise<AirStation[]> => api.post('/app/data/gis-data/air-station', query),
 
-  // 驾驶舱-近岸海域(右上)
+  /**
+   * 驾驶舱-近岸海域(右上)
+   * @param query
+   * @returns
+   */
   getSeaWaterOverall: (query: GeneralQuery): Promise<SeaWaterOverallStats[]> => api.post('/app/data/gis-data/sea-water-overall', query),
 
-  // 驾驶舱-气象实况(右下)
+  /**
+   * 驾驶舱-气象实况(右下)
+   * @param query
+   * @returns
+   */
   getMeteorology: (query: GeneralQuery): Promise<Meteorology[]> => api.post('/app/data/gis-data/meteorology', query),
 
-  // 入海河流-河流水质达标考核
+  /**
+   * 入海河流-河流水质达标考核
+   * @param query
+   * @returns
+   */
   getWaterQualityComplianceResult: (query: GeneralQuery): Promise<WaterQualityComplianceResult> => api.post('/app/data/gis-data/water-compliance', query),
 
-  // 入海河流-水质现状统计分析
+  /**
+   * 入海河流-水质现状统计分析
+   * @param query
+   * @returns
+   */
   getWaterYoyProportion: (query: GeneralQuery): Promise<WaterYoyProportion> => api.post('/app/data/gis-data/water-yoy', query),
 
+  /**
+   * 入海河流-水质达标率趋势分析
+   * @param query
+   * @returns
+   */
+  getWaterQualityTrend: (query: GeneralQuery): Promise<WaterQualityTrend> => api.post('/app/data/gis-data/water-trend', query),
+
+  /**
+   * 入海河流-断面监测详情
+   * @param query
+   * @returns
+   */
+  getWaterQualityDetail: (query: GeneralQuery): Promise<WaterQualityDetail> => api.post('/app/data/gis-data/water-wq-detal', query),
+
+  /**
+   * 入海排污口-入海排污口统计
+   * @param query
+   * @returns
+   */
+  getOutfallOverallStats: (query: GeneralQuery): Promise<OutfallOverallStats> => api.post('/app/data/gis-data/outfall-stat', query),
+
+  /**
+   * 入海排污口-收纳水体排污口类型分析
+   * @param query
+   * @returns
+   */
+  getOutfallTypeStats: (query: GeneralQuery): Promise<OutfallTypeStats> => api.post('/app/data/gis-data/outfall-type-stat', query),
+
+  /**
+   * 入海排污口-收纳水体目标统计
+   * @param query
+   * @returns
+   */
+  getOutfallGoalStats: (query: GeneralQuery): Promise<OutfallGoalStats> => api.post('/app/data/gis-data/outfall-goal-stat', query),
+
+  /**
+   * 近岸海域-近岸海域水质达标考核（左上）
+   * @param query
+   * @returns
+   */
+  getSeaWaterQualityCompliance: (query: GeneralQuery): Promise<SeaWaterQualityCompliance[]> => api.post('/app/data/gis-data/sea-compliance', query),
+
+  /**
+   * 近岸海域-水质现状分析（左下）
+   * @param query
+   * @returns
+   */
+  getSeaWaterStats: (query: GeneralQuery): Promise<SeaWaterStats> => api.post('/app/data/gis-data/sea-stats', query),
+
+  /**
+   * 近岸海域-近岸水域面积变化趋势(右上)
+   * @param query
+   * @returns
+   */
+  getSeaWaterQualityTrend: (query: GeneralQuery): Promise<SeaWaterQualityTrend> => api.post('/app/data/gis-data/sea-water-trend', query),
+
+  /**
+   * 近岸海域-富营养化（右下）
+   * @param query
+   * @returns
+   */
+  getSeaWaterEutrophication: (query: GeneralQuery): Promise<SeaWaterEutrophication> => api.post('/app/data/gis-data/sea-water-e', query),
 }
--- src/api/modules/resultTypes.ts
+++ src/api/modules/resultTypes.ts
@@ -7,8 +7,8 @@ export interface ProvinceCityTree {
 }
 
 /**
-   * 驾驶舱-入海河流,每个区域统计对象
-   */
+ * 驾驶舱-入海河流,每个区域统计对象
+ */
 export interface OverallWaterQualityStats {
   time?: string // 时间
   sectionCount: number // 监测断面数量
@@ -36,7 +36,7 @@ export interface SeaWaterOverallStats {
   currentMonthTotal: number // 总数或者总面积
   // 各个省份数据
   provinceResults: {
-    province: string// 省份
+    province: string // 省份
     target: number //  目标
     targetDifference: number // 优于目标
   }[]
@@ -46,11 +46,11 @@ export interface SeaWaterOverallStats {
  * 驾驶舱-气象实况
  */
 export interface Meteorology {
-  stationName: string// 站点
-  winDirection: string// 风速
-  winSpeed: string// 风向
-  RHU: string// 湿度
-  dateTime: string// 监测时间
+  stationName: string // 站点
+  winDirection: string // 风速
+  winSpeed: string // 风向
+  RHU: string // 湿度
+  dateTime: string // 监测时间
 }
 
 /**
@@ -71,21 +71,152 @@ export interface WaterQualityComplianceResult {
  * 入海河流-水质现状统计分析
  */
 export interface WaterYoyProportion {
-  header: string[]// 表头[9月，1-9月],1月份的话长度就是1
+  header: string[] // 表头[9月，1-9月],1月份的话长度就是1
   data: {
     // 1-3类水质或者达标,数组第一个代表当月，第二个代表累计类似1-9月
     good: {
-      currrentMon: number// 当前优良水质占比
-      yoyRate: number// 优良水质同比
-      currrentTnMon: number// 当前总氮达标占比
-      yoyTnRate: number// 总氮达标同比
+      currrentMon: number // 当前优良水质占比
+      yoyRate: number // 优良水质同比
+      currrentTnMon: number // 当前总氮达标占比
+      yoyTnRate: number // 总氮达标同比
     }[]
     // 4类水质或者不达标,数组第一个代表当月，第二个代表累计类似1-9月
     poor: {
-      currrentMon: number// 当前不达标水质占比
-      yoyRate: number// 不达标水质同比
-      currrentTnMon: number// 当前总氮不达标占比
-      yoyTnRate: number// 总氮不达标同比
+      currrentMon: number // 当前不达标水质占比
+      yoyRate: number // 不达标水质同比
+      currrentTnMon: number // 当前总氮不达标占比
+      yoyTnRate: number // 总氮不达标同比
     }[]
   }
+}
+
+/**
+ * 入海河流-水质达标率趋势分析
+ */
+export interface WaterQualityTrend {
+  months: string[] // 月份
+  waterQualityComplianceRates: number[] // 达标率
+  waterQualityNonComplianceRates: number[] // 不达标率
+  waterQualityYoYChanges: number[] // 达标率同比
+
+  totalNitrogenComplianceRates: number[] // 总氮达标率
+  totalNitrogenNonComplianceRates: number[] // 总氮不达标率
+  totalNitrogenYoYChanges: number[] // 总氮达标率同比
+}
+
+/**
+ * 入海河流-断面监测详情
+ */
+export interface WaterQualityDetail {
+  province: string // 省份
+  city: string // 城市
+  selectCode: string // 断面code
+  sectionName: string // 断面名称
+  waterType: string // 水质类别
+  waterTarget: string // 目标类别
+  isCompliant: boolean // 是否达标
+  waterChange: number // 变化差值
+  lastMotnType: string // 上月水质类型
+  lat: string // 维度
+}
+
+/**
+ * 入海排污口-入海排污口统计
+ */
+export interface OutfallOverallStats {
+  name: string // 标题
+  count: number // 排污口总数
+  chartData: {
+    industry: string[] // 行业
+    industryCount: number[] // 行业数量
+  }
+}
+
+/**
+ * 入海排污口-收纳水体排污口类型分析
+ */
+export interface OutfallTypeStats {
+  name: string // 标题
+  count: number // 排污口总数
+  chartData: {
+    type: string[] // 排污口类型
+    typeCount: number[] // 排污口数量
+  }
+}
+
+/**
+ * 入海排污口-收纳水体目标统计
+ */
+export interface OutfallGoalStats {
+  name: string // 标题
+  count: number // 排污口总数
+  // 详细数据
+  data: {
+    waterQualityGoal: string // 目标水体类别
+    count: number // 总数
+    rate: number // 占比
+    exceeCount: number // 超标数量
+    exceeRate: number // 超标比例
+  }[]
+}
+
+/**
+ * 近岸海域-近岸海域水质达标考核（左上）
+ */
+export interface SeaWaterQualityCompliance {
+  province: string
+  seaWaterComplianceResult: {
+    count: number
+    currentSeasonComplianceRate: number
+    lastYearDifference: number
+    year2020ComplianceRate: number
+    targetComplianceRate: number
+    isCompliance: boolean
+  }[]
+}
+
+/**
+ * 近岸海域-水质现状分析（左下）
+ */
+export interface SeaWaterStats {
+  season: string // 季节
+  area: {
+    currentSeasonClass1And2Rate: number // 本季节1-2类水质占比
+    class1And2SeasonDifference: number // 1-2类水质环比
+    currentYearClass1And2Rate: number // 年度累计1-2水质占比
+    class1And2YearDifference: number // 年度1-2类水质环比
+    currentSeasonAboveClass4Rate: number // 本季节4类水质占比
+    aboveClass4SeasonDifference: number // 本季节4类水质环比
+    currentYearAboveClass4Rate: number // 年度4类水质占比
+    aboveClass4YearDifference: number // 年度4类水质环比
+  }
+  point: {
+    currentSeasonClass1And2Rate: number // 本季节1-2类水质占比
+    class1And2SeasonDifference: number // 1-2类水质环比
+    currentYearClass1And2Rate: number // 年度累计1-2水质占比
+    class1And2YearDifference: number // 年度1-2类水质环比
+    currentSeasonAboveClass4Rate: number // 本季节4类水质占比
+    aboveClass4SeasonDifference: number // 本季节4类水质环比
+    currentYearAboveClass4Rate: number // 年度4类水质占比
+    aboveClass4YearDifference: number // 年度4类水质环比
+  }
+}
+
+/**
+ * 近岸海域-近岸水域面积变化趋势(右上)
+ */
+export interface SeaWaterQualityTrend {
+  seasons: string[] // 季节
+  waterQualityComplianceRates: number[] // 达标率
+  waterQualityQoqChanges: number[] // 环比
+  waterQualityYoYChanges: number[] // 同比
+}
+
+/**
+ * 近岸海域-富营养化（右下）
+ */
+export interface SeaWaterEutrophication {
+  names: string[] // 富营养化级别
+  data: number[] // 数量
+  color: string[] // 颜色
 }
